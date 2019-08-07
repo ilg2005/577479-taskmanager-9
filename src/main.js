fc7b67e93b0ff03703972c2fd8a70f89e3560ef0
ilg2005 <igoldfarb10@gmail.com>
2019-08-07 10:01:31 +0300

    функция, возвращающая разметку фильтров сортировки карточек

--- src/main.js
+++ src/main.js
@@ -114,3 +114,13 @@ const getFiltersMarkup = () => (`
       </section>
 `);
 
+const getTaskboardFiltersMarkup = () => (`
+<section class="board container">
+        <div class="board__filter-list">
+          <a href="#" class="board__filter">SORT BY DEFAULT</a>
+          <a href="#" class="board__filter">SORT BY DATE up</a>
+          <a href="#" class="board__filter">SORT BY DATE down</a>
+        </div>
+        </section>
+`);
+
