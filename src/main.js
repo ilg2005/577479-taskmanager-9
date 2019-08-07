969de8064fcb639f2b89a494f6f15353ac728068
ilg2005 <igoldfarb10@gmail.com>
2019-08-07 09:47:49 +0300

    функция, возвращающая разметку поискового контейнера

--- src/main.js
+++ src/main.js
@@ -1,32 +1,46 @@
 'use strict';
 
 const getMenuMarkup = () => (`
-<section class="control__btn-wrap">
-  <input
-type="radio"
-name="control"
-id="control__new-task"
-class="control__input visually-hidden"
-  />
-  <label for="control__new-task" class="control__label control__label--new-task"
-  >+ ADD NEW TASK</label
->
-<input
-type="radio"
-name="control"
-id="control__task"
-class="control__input visually-hidden"
-checked
-/>
-<label for="control__task" class="control__label">TASKS</label>
-  <input
-type="radio"
-name="control"
-id="control__statistic"
-class="control__input visually-hidden"
-  />
-  <label for="control__statistic" class="control__label"
-  >STATISTICS</label
-  >
-  </section>
+        <section class="control__btn-wrap">
+          <input
+            type="radio"
+            name="control"
+            id="control__new-task"
+            class="control__input visually-hidden"
+          />
+          <label for="control__new-task" class="control__label control__label--new-task"
+            >+ ADD NEW TASK</label
+          >
+          <input
+            type="radio"
+            name="control"
+            id="control__task"
+            class="control__input visually-hidden"
+            checked
+          />
+          <label for="control__task" class="control__label">TASKS</label>
+          <input
+            type="radio"
+            name="control"
+            id="control__statistic"
+            class="control__input visually-hidden"
+          />
+          <label for="control__statistic" class="control__label"
+            >STATISTICS</label
+          >
+        </section>
 `);
+
+const getSearchContainerMarkup = () => (`
+<section class="main__search search container">
+        <input
+          type="text"
+          id="search__input"
+          class="search__input"
+          placeholder="START TYPING — SEARCH BY WORD, #HASHTAG OR DATE"
+        />
+        <label class="visually-hidden" for="search__input">Search</label>
+      </section>
+`);
+
+
