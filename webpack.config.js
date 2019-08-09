cce73e3a70e94f7f952c8a3bc9741338e3470d3a
ilg2005 <igoldfarb10@gmail.com>
2019-08-09 18:24:44 +0300

    исправлен publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   devtool: `source-map`,
   devServer: {
     contentBase: path.join(__dirname, `public`),
-    publicPath: `http://localhost:8080/webpack-dev-server/`,
+    publicPath: `http://localhost:63342/577479-taskmanager-9/public/index.html`,
     compress: true,
     watchContentBase: true
   }
