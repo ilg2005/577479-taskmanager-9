0ff01a62c9ef1e5a0ff3991caa377edc3c1af099
ilg2005 <igoldfarb10@gmail.com>
2019-08-09 16:41:07 +0300

    Модули подключены в main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+import {getMenuMarkup} from './js/components/menu.js';
+import {getSearchFieldMarkup} from './js/components/searchfield.js';
+import {getFiltersMarkup} from './js/components/filter.js';
+import {getTaskboardContainerMarkup} from './js/components/taskboard-container.js';
+import {getCardEditFormMarkup} from './js/components/card-edit-form.js';
+import {getCardMarkup} from './js/components/card.js';
+import {getLoadMoreBtnMarkup} from './js/components/load-more-btn.js';
+
 const renderElement = (element, markup, renderingCount = 1) => {
   for (let i = 1; i <= renderingCount; i++) {
     element.insertAdjacentHTML(`beforeend`, markup);
