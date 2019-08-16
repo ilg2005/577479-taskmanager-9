9ce7a17357181b1f5f49304dbac664393f09a725
ilg2005 <igoldfarb10@gmail.com>
2019-08-16 16:57:49 +0300

    исправлен перевод времени в 12 ч формат

--- src/components/card.js
+++ src/components/card.js
@@ -1,9 +1,9 @@
 const MONTHS = [`JANUARY`, `FEBRUARY`, `MARCH`, `APRIL`, `MAY`, `JUNE`, `JULY`, `AUGUST`, `SEPTEMBER`, `OCTOBER`, `NOVEMBER`, `DECEMBER`];
 
 const getTimePeriod = (hour) => (hour >= 12) ? `PM` : `AM`;
-const getHourIn12hFormat = (hour) => (hour >= 12) ? (hour - 12) : hour;
+const getHourIn12hFormat = (hour) => (hour > 12) ? (hour - 12) : hour;
 
-const checkOverdue = (deadline) => (deadline < new Date()) ? `card--deadline` : ``;
+const checkOverdue = (deadline) => (deadline <= new Date()) ? `card--deadline` : ``;
 
 export const getCardMarkup = (color, repeatingDays, description, dueDate, tags) => (`
           <article class="card card--${color} ${Object.keys(repeatingDays).some((day) => repeatingDays[day]) ? `card--repeat` : ``} ${checkOverdue(dueDate)}">
