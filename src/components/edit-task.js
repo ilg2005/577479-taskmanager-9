d928b03a1fcef9812e1f0b361c96dbf732109608
ilg2005 <igoldfarb10@gmail.com>
2019-08-18 04:16:00 +0300

    repeatingDays в карточку редактирования загружаются

--- src/components/edit-task.js
+++ src/components/edit-task.js
@@ -57,79 +57,18 @@ export const getCardEditFormMarkup = (color = `black`, repeatingDays, descriptio
 
                       <fieldset class="card__repeat-days">
                         <div class="card__repeat-days-inner">
+                        ${Object.keys(repeatingDays).map((day) => `
                           <input
                             class="visually-hidden card__repeat-day-input"
                             type="checkbox"
-                            id="repeat-mo-4"
+                            id="repeat-${day}-4"
                             name="repeat"
-                            value="mo"
+                            value=${day}
+                            ${repeatingDays[day] ? `checked` : ``}
                           />
-                          <label class="card__repeat-day" for="repeat-mo-4"
-                            >mo</label
-                          >
-                          <input
-                            class="visually-hidden card__repeat-day-input"
-                            type="checkbox"
-                            id="repeat-tu-4"
-                            name="repeat"
-                            value="tu"
-                            checked
-                          />
-                          <label class="card__repeat-day" for="repeat-tu-4"
-                            >tu</label
-                          >
-                          <input
-                            class="visually-hidden card__repeat-day-input"
-                            type="checkbox"
-                            id="repeat-we-4"
-                            name="repeat"
-                            value="we"
-                          />
-                          <label class="card__repeat-day" for="repeat-we-4"
-                            >we</label
-                          >
-                          <input
-                            class="visually-hidden card__repeat-day-input"
-                            type="checkbox"
-                            id="repeat-th-4"
-                            name="repeat"
-                            value="th"
-                          />
-                          <label class="card__repeat-day" for="repeat-th-4"
-                            >th</label
-                          >
-                          <input
-                            class="visually-hidden card__repeat-day-input"
-                            type="checkbox"
-                            id="repeat-fr-4"
-                            name="repeat"
-                            value="fr"
-                            checked
-                          />
-                          <label class="card__repeat-day" for="repeat-fr-4"
-                            >fr</label
-                          >
-                          <input
-                            class="visually-hidden card__repeat-day-input"
-                            type="checkbox"
-                            name="repeat"
-                            value="sa"
-                            id="repeat-sa-4"
-                          />
-                          <label class="card__repeat-day" for="repeat-sa-4"
-                            >sa</label
-                          >
-                          <input
-                            class="visually-hidden card__repeat-day-input"
-                            type="checkbox"
-                            id="repeat-su-4"
-                            name="repeat"
-                            value="su"
-                            checked
-                          />
-                          <label class="card__repeat-day" for="repeat-su-4"
-                            >su</label
-                          >
+                          <label class="card__repeat-day" for="repeat-${day}-4"
+                            >${day}</label
+                          >`).join(``)}
                         </div>
                       </fieldset>
                     </div>
