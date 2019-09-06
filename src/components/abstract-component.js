4c3e63c4da9cfb443cb1bead606d45d286b12d80
ilg2005 <igoldfarb10@gmail.com>
2019-09-06 13:36:39 +0300

    создан абстрактный класс

---
+++ src/components/abstract-component.js
@@ -0,0 +1,25 @@
+import {utils} from "./utils.js";
+
+export default class AbstractComponent {
+  constructor() {
+    if (new.target === AbstractComponent) {
+      throw new Error(`Can't instantiate AbstractComponent, only concrete one.`);
+    }
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = utils.createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    throw Error(`Abstract method not implemented`);
+  }
+}
