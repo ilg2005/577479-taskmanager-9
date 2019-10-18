71e0954c164cb9f969db3084dadcaa761c471065
ilg2005 <igoldfarb10@gmail.com>
2019-10-18 17:38:43 +0300

    текущее состояние

--- src/components/card/card-hashtags.js
+++ src/components/card/card-hashtags.js
@@ -6,7 +6,7 @@ export default class CardHashtags extends Abstract {
   constructor(tags) {
     super();
     this._tags = tags;
-    this._container = this.getElement().querySelector(`input`);
+    this._inputContainer = this.getElement().querySelector(`.card__hashtag-input`);
     this._tagsNumber = tags.size;
     this._changeHashtags();
   }
@@ -34,33 +34,26 @@ export default class CardHashtags extends Abstract {
 
     for (let [rule, value] of Object.entries(rules)) {
       if (value) {
-        console.log(descriptions[rule]);
-        this._container.setCustomValidity(descriptions[rule]);
+        this._inputContainer.setCustomValidity(descriptions[rule]);
       }
     }
-
-    return this._container.validity.valid;
+    return this._inputContainer.validity.valid;
   }
 
 
   _changeHashtags() {
-    const hashtagsElement = this.getElement();
-    const hashtagsInputElement = hashtagsElement.querySelector(`.card__hashtag-input`);
-
     const hashtagsInputElementKeydownHandler = (evt) => {
       if (evt.key === `Space` || evt.keyCode === 32 || evt.key === `Enter` || evt.keyCode === 13) {
         evt.preventDefault();
-        const hashtagInput = hashtagsInputElement.value;
+        let hashtagInput = this._inputContainer.value;
         const strippedHashtag = striptags(hashtagInput).trim().replace(/^#+/, ``);
         if (this._validateHashtag(strippedHashtag)) {
           this._tags.add(strippedHashtag);
           const newTagsElement = new CardHashtags(this._tags);
           this.replace(newTagsElement);
-
-          hashtagsInputElement.value = ``;
         }
       } else {
-        this._container.setCustomValidity(``);
+        this._inputContainer.setCustomValidity(``);
       }
     };
 
@@ -71,12 +64,12 @@ export default class CardHashtags extends Abstract {
         const strippedTag = tag.trim().replace(/^#+/, ``);
         this._tags.delete(strippedTag);
         utils.unrender(tagContainer);
-        hashtagsInputElement.setCustomValidity(``);
+        this._inputContainer.setCustomValidity(``);
       }
     };
 
-    hashtagsElement.addEventListener(`click`, hashtagsElementClickHandler);
-    hashtagsInputElement.addEventListener(`keydown`, hashtagsInputElementKeydownHandler);
+    this.getElement().addEventListener(`click`, hashtagsElementClickHandler);
+    this._inputContainer.addEventListener(`keydown`, hashtagsInputElementKeydownHandler);
   }
 
   getTemplate() {
