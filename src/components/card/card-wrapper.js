71c84e8713c215b8827e87278f5860571852a4c0
ilg2005 <igoldfarb10@gmail.com>
2019-10-19 16:29:41 +0300

    текущее состояние

--- src/components/card/card-wrapper.js
+++ src/components/card/card-wrapper.js
@@ -1,13 +1,14 @@
 import Abstract from "../abstract.js";
 
 export default class CardWrapper extends Abstract {
-  constructor(task = []) {
+  constructor(color, id) {
     super();
-    this._task = task;
+    this._color = color;
+    this._id = id;
   }
 
   getTemplate() {
-    return `<article class="card card--edit card--${this._task.color}" data-card="${this._task.id}">
+    return `<article class="card card--edit card--${this._color}" data-card="${this._id}">
                 <form class="card__form" method="get">
                 <div class="card__inner">
                     
